4ccb1f5b245d0f0a3dd7eca912975066ecc56ae0
Turboman3000 <turboman@turboman.de>
2025-01-14 17:43:02 +0100

    fix: register block handlers

--- src/main/java/de/turboman/zombies/Zombies.java
+++ src/main/java/de/turboman/zombies/Zombies.java
@@ -1,25 +1,34 @@
 package de.turboman.zombies;
 
-import de.turboman.zombies.handlers.*;
+import de.turboman.zombies.handlers.BannerHandler;
+import de.turboman.zombies.handlers.DummyHandler;
+import de.turboman.zombies.handlers.SignHandler;
+import de.turboman.zombies.handlers.SkullHandler;
 import net.hollowcube.polar.PolarLoader;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
-import net.minestom.server.gamedata.tags.Tag;
+import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Objects;
 
 public class Zombies {
+
+    private static final Logger log = LoggerFactory.getLogger(Zombies.class);
 
     public static void main(String[] args) throws IOException {
         MinecraftServer minecraftServer = MinecraftServer.init();
 
+        MinecraftServer.getBlockManager().registerHandler("minecraft:comparator", DummyHandler::new);
+        MinecraftServer.getBlockManager().registerHandler("minecraft:command_block", DummyHandler::new);
         MinecraftServer.getBlockManager().registerHandler("minecraft:daylight_detector", DummyHandler::new);
         MinecraftServer.getBlockManager().registerHandler("minecraft:campfire", DummyHandler::new);
         MinecraftServer.getBlockManager().registerHandler("minecraft:beehive", DummyHandler::new);
@@ -28,21 +37,11 @@ public class Zombies {
         MinecraftServer.getBlockManager().registerHandler("minecraft:furnace", DummyHandler::new);
         MinecraftServer.getBlockManager().registerHandler("minecraft:chest", DummyHandler::new);
 
+        MinecraftServer.getBlockManager().registerHandler("minecraft:skull", SkullHandler::new);
         MinecraftServer.getBlockManager().registerHandler("minecraft:player_head", SkullHandler::new);
         MinecraftServer.getBlockManager().registerHandler("minecraft:banner", BannerHandler::new);
         MinecraftServer.getBlockManager().registerHandler("minecraft:sign", SignHandler::new);
-
-        for (var value : Objects.requireNonNull(MinecraftServer.getTagManager().getTag(Tag.BasicType.BLOCKS, "minecraft:all_signs")).getValues()) {
-            MinecraftServer.getBlockManager().registerHandler(value, SignHandler::new);
-        }
-
-        for (var value : Objects.requireNonNull(MinecraftServer.getTagManager().getTag(Tag.BasicType.BLOCKS, "minecraft:wooden_doors")).getValues()) {
-            MinecraftServer.getBlockManager().registerHandler(value, DoorHandler::new);
-        }
-
-        for (var value : Objects.requireNonNull(MinecraftServer.getTagManager().getTag(Tag.BasicType.BLOCKS, "minecraft:wooden_trapdoors")).getValues()) {
-            MinecraftServer.getBlockManager().registerHandler(value, TrapdoorHandler::new);
-        }
+        MinecraftServer.getBlockManager().registerHandler("minecraft:hanging_sign", SignHandler::new);
 
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
         InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
@@ -53,8 +52,15 @@ public class Zombies {
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             final Player player = event.getPlayer();
+
             event.setSpawningInstance(instanceContainer);
-            player.setRespawnPoint(new Pos(0, 142, 0));
+            player.setRespawnPoint(new Pos(0, -55, 0));
+        });
+
+        globalEventHandler.addListener(PlayerSpawnEvent.class, event -> {
+            final Player player = event.getPlayer();
+
+            player.setGameMode(GameMode.ADVENTURE);
         });
 
         minecraftServer.start("0.0.0.0", 25565);
